aaea4bc7fee494dde6335b03bb00b289975b98bc
Luo Yu <luoyu@luoyu.space>
2022-03-25 11:56:48 +0800

    [Modify] demo

--- Example/LYFactAppCommon/_scene/views/DarkShowNavVC.m
+++ Example/LYFactAppCommon/_scene/views/DarkShowNavVC.m
@@ -27,7 +27,7 @@
 #import "DarkShowNavVC.h"
 
 
-@interface DarkShowNavVC () {}
+@interface DarkShowNavVC () <UITableViewDelegate, UITableViewDataSource> {}
 @end
 
 @implementation DarkShowNavVC
@@ -42,6 +42,54 @@
 	[super loadView];
 	
 	self.view.backgroundColor = [UIColor darkGrayColor];
+	
+	{
+		FACBaseTableView *view = [[FACBaseTableView alloc] init];
+		view.translatesAutoresizingMaskIntoConstraints = NO;
+		[self.view addSubview:view];
+		
+		[view registerClass:[FACBaseTableCell class] forCellReuseIdentifier:FACBaseTableCellIdentifier];
+		[view registerClass:[FACChatBaseLeftCell class] forCellReuseIdentifier:@"FACChatBaseLeftCellIdentifier"];
+		[view registerClass:[FACChatBaseRightCell class] forCellReuseIdentifier:@"FACChatBaseRightCellIdentifier"];
+		
+		view.delegate = self;
+		view.dataSource = self;
+		
+		[view.leftAnchor constraintEqualToAnchor:self.view.leftAnchor].active = YES;
+		[view.rightAnchor constraintEqualToAnchor:self.view.rightAnchor].active = YES;
+		[view.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor].active = YES;
+		[view.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
+	}
+}
+
+// MARK: - DELEGATE
+
+// MARK: UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)idp {
+	[tableView deselectRowAtIndexPath:idp animated:YES];
+}
+
+// MARK: UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	NSInteger rows = 20;
+	return rows;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)idp {
+	
+	if (idp.row % 2 == 0) {
+		FACChatBaseLeftCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FACChatBaseLeftCellIdentifier" forIndexPath:idp];
+		cell.lblContent.text = [NSString stringWithFormat:@"Left %@ %@", @(idp.row), @(cell.side)];
+		return cell;
+	} else {
+		FACChatBaseRightCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FACChatBaseRightCellIdentifier" forIndexPath:idp];
+		cell.lblContent.text = [NSString stringWithFormat:@"Right %@ %@", @(idp.row), @(cell.side)];
+		return cell;
+	}
+	
+	return [tableView dequeueReusableCellWithIdentifier:FACBaseTableCellIdentifier forIndexPath:idp];
 }
 
 @end
